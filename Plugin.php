2f043a627079ef663a8acc274bab839a9da08578
Sam Georges <sam@daftspunk.com>
2013-12-07 09:13:04 +1100

    Create plugin info file.

---
+++ Plugin.php
@@ -0,0 +1,47 @@
+<?php namespace Plugins\Responsiv\DishSmith;
+
+use Backend;
+use Modules\System\Classes\PluginBase;
+
+class Plugin extends PluginBase
+{
+
+    public function pluginDetails()
+    {
+        return [
+            'name' => 'DishSmith',
+            'description' => 'DishSmith specific features.',
+            'author' => 'Samuel Georges',
+            'icon' => 'icon-food'
+        ];
+    }
+
+    public function registerComponents()
+    {
+        return [
+            // 'Plugins\October\User\Components\Register' => 'userRegister',
+        ];
+    }
+
+    public function registerNavigation()
+    {
+        return [
+            'user' => [
+                'label' => 'DishSmith',
+                'url' => Backend::url('dishsmith/dishes'),
+                'icon' => 'icon-food',
+                'permissions' => ['dishsmith:*'],
+                'order' => 500,
+                'sideMenu' => [
+                    'users' => [
+                        'label' => 'All Users',
+                        'icon' => 'icon-food',
+                        'url' => Backend::url('responsiv/dishsmith/dishes'),
+                        'permissions' => ['dishsmith:access_dishes'],
+                    ],
+                ]
+            ]
+        ];
+    }
+
+}
