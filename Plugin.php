185cbb3d69f35b6c93417b49524cee09af419053
Sam Georges <sam@daftspunk.com>
2014-03-22 16:17:19 +1100

    Beautify code

--- Plugin.php
+++ Plugin.php
@@ -9,38 +9,10 @@ class Plugin extends PluginBase
     public function pluginDetails()
     {
         return [
-            'name' => 'DishSmith',
+            'name'        => 'DishSmith',
             'description' => 'DishSmith specific features.',
-            'author' => 'Samuel Georges',
-            'icon' => 'icon-food'
-        ];
-    }
-
-    public function registerComponents()
-    {
-        return [
-            // 'RainLab\User\Components\Register' => 'userRegister',
-        ];
-    }
-
-    public function registerNavigation()
-    {
-        return [
-            // 'dishsmith' => [
-            //     'label' => 'DishSmith',
-            //     'url' => Backend::url('dishsmith/dishes'),
-            //     'icon' => 'icon-food',
-            //     'permissions' => ['dishsmith:*'],
-            //     'order' => 500,
-            //     'sideMenu' => [
-            //         'users' => [
-            //             'label' => 'All Users',
-            //             'icon' => 'icon-food',
-            //             'url' => Backend::url('responsiv/dishsmith/dishes'),
-            //             'permissions' => ['dishsmith:access_dishes'],
-            //         ],
-            //     ]
-            // ]
+            'author'      => 'Samuel Georges',
+            'icon'        => 'icon-food'
         ];
     }
 
